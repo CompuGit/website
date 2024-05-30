e14031f52c88f8168d8917dd541f5ccf7010918d
poorna <ad.compugit@gmail.com>
2024-05-30 21:44:07 +0530

    test

--- src/app/pages/developer/quickstart/quickstart.component.ts
+++ src/app/pages/developer/quickstart/quickstart.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { RightnavsectionComponent } from '../../../components/rightnavsection/rightnavsection.component';
-import { NgFor } from '@angular/common';
+
+import { MainComponent } from '../../../components/main/main.component';
 
 @Component({
   selector: 'app-quickstart',
   standalone: true,
-  imports: [RightnavsectionComponent, NgFor],
+  imports: [MainComponent,],
   templateUrl: './quickstart.component.html',
   styleUrl: './quickstart.component.css'
 })
+
 export class QuickstartComponent  implements OnInit{
 
   nav_list : any = []
